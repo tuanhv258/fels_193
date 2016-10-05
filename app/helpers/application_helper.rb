4352266be4a8f28d0110fa3b8b49f902729a2b0b
canvantuan <canvantuan12@gmail.com>
2016-10-05 17:18:54 +0700

    Update 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def corrected_user
+    @user = User.find_by params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "page.usercontroller.loggeddanger"
+      redirect_to login_url
+    end
+  end
+
 end
