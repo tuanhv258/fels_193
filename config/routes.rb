1bfcea41efee67cf45f7dd47a46794366aeaecc0
tuanhv258 <tuanhv123@gmail.com>
2016-10-07 13:29:53 +0700

    Fix comment show and update 2nd

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
-  post "/signup",  to: "users#create"
-  resources :users, :only => ["new", "create", "new", "show", "index"]
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
+  resources :users
 end
--- db/migrate/20161005020448_create_users.rb
+++ db/migrate/20161005020448_create_users.rb
@@ -2,10 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email
-      t.string :password_digest
       t.string :fullname
       t.string :avatar
-      t.boolean :is_admin, default :false
+      t.boolean :is_admin, default: false
       t.timestamps null: false
     end
   end
