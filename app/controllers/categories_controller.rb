bce493a276e3941c649e6b80f51a4a1d5703fa08
canvantuan <canvantuan12@gmail.com>
2016-10-10 16:32:20 +0700

    Create Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,29 @@
 class CategoriesController < ApplicationController
   before_action :check_logged
+
   def index
     @categories = Category.paginate(page: params[:page],
       per_page: Settings.categories_per_page).order(created_at: "desc")
   end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new category_params
+    if @categories.save
+      flash[:success] = t "page.categoriescontroller.createsuccess"
+      redirect_to categories_path
+    else
+      flash[:danger] = t "page.categoriescontroller.createdanger"
+      render :new
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :title, :detail
+  end
+
 end
