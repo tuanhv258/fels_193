44da18b790f6346a6cbf98181f839a3098ef2369
canvantuan <canvantuan12@gmail.com>
2016-10-10 13:46:41 +0700

    Delete User

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,16 @@
 class Admin::UsersController < ApplicationController
-  before_action :logged_in_user, :required_admin, only: [:index]
+  before_action :logged_in_user, :required_admin,
+    only: [:index, :edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page],
-      per_page: Settings.categories_per_page).order(created_at: "desc")
+      per_page: Settings.categories_per_page).order created_at: "desc"
+  end
+
+  def destroy
+    loaduser.destroy
+    flash[:success] = t "page.usercontroller.deletedestroy"
+    redirect_to users_url
   end
 
 end
