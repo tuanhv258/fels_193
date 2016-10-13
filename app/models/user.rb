5236fa3cfdbca612edc2bb15d8dab8b5941e914f
canvantuan <canvantuan12@gmail.com>
2016-10-13 10:53:40 +0700

    Follow-Unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ActiveRecord::Base
   has_many :lessons
   has_many :relationships
   has_many :activities
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id",
+    dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+    foreign_key: "followed_id",
+    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   before_save {self.email = email.downcase}
 
   validates :fullname, presence: true, length: {maximum: 255}
@@ -39,6 +48,18 @@ class User < ActiveRecord::Base
 
   def current_user? current_user
     self == current_user
+  end
+
+  def follow other_user
+    active_relationships.create followed_id: other_user.id
+  end
+
+  def unfollow other_user
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 
 end
