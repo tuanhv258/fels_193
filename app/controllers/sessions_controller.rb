b9f81d53128fb806ee89c48b1b9c5aa8fb85cb5d
tuanhv258 <tuanhv123@gmail.com>
2016-10-17 21:18:29 +0700

    Refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       flash[:success] = t "page.sessioncontroller.success"
-      redirect_to user
+      redirect_to categories_path
     else
       flash[:danger] = t "page.sessioncontroller.danger"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :load_user, only: [:show, :edit, :update]
+  before_action :load_user, except: [:new, :create, :destroy]
 
   def index
     @users = User.paginate(page: params[:page],
